7ea7e35970f9222c0a5e4460b57643b24ee9eb0f
jananiiramesh <reachjanu2004@gmail.com>
2026-01-10 11:27:50 +0530

    Added the input logic

---
+++ app/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route("/classify", methods=["POST"])
+def classify_input():
+    if request.content_type == "text/plain":
+        text = request.get_data(as_text=True)
+        return jsonify({
+            "input_type": "raw text",
+            "content": text
+        })
+    
+    # if request.is_json:
+    #     data = request.get_json()
+    #     if "text" in data:
+    #         return jsonify({
+    #             "input_type":"text",
+    #             "content": data["text"]
+    #         })
+        
+    elif "image" in request.files:
+        image_file = request.files["image"]
+
+        try:
+            image = Image.open(io.BytesIO(image_file.read()))
+            return jsonify({
+                "input_type":"image",
+                "image_format": image.format,
+                "image_size": image.size
+            })
+        
+        except Exception as e:
+            return jsonify({"error":"Invalid image"}), 400
+        
+    return jsonify({"error": "Unsupported input type"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
